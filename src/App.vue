01ec3eb71cc3ec32b872e42d7e178f7c6cc42381
waiyan <wy04839@gmail.com>
2022-06-14 23:06:22 +0630

    multi-word Error

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello</h1>
+  <Modal/>
+</template>
+
+<script>
+
+import Modal from "./Modal.vue"
+export default {
+  components:{
+    Modal
+  }
+}
+</script>
+
+<style>
+  h1{
+    color: red;
+  }
+</style>
---
+++ src/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+        <h2>Login Successful</h2>
+        <p>Welcome to our website.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .backdrop{
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+    }
+
+    .modal{
+        width: 300px;
+        height: 100px;
+        background-color: seagreen;
+        padding: 20px;
+        margin: 20px auto;
+    }
+</style>
